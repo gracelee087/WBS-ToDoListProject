aaa120dcad8c6def056553ddc305c8b3f1ca89c7
elvisbevab <elvis.bevab@mailbox.org>
2023-05-09 18:27:41 +0200

    adding margin in offcanvas between searchbar and home for nicer look

--- js/elvis.js
+++ js/elvis.js
@@ -1,24 +1,41 @@
 
 // Consts and Variables
+// Navbar Items Right Bar
 const rightOffcanvasToggler = document.querySelector("#rightOffcanvasToggler");
+const rightOffcanvas = document.querySelector("#tdNavbar");
+// console.log(rightOffcanvas);
 const leftCollapseToggler = document.querySelector("#leftCollapseToggler");
+const homeItem = document.querySelector("#homeItem")
+
+//Navbar left Side
 const leftSideBar = document.querySelector("#leftSideBar");
-let largeScreen = true;
+
+const rightNavbarBreakpoint = 992;
+let largeScreen = window.innerWidth >= rightNavbarBreakpoint ? true : false;
 
 //Event Listeners
 leftCollapseToggler.addEventListener("click", (e) => {
   console.log(e.target);
 });
 
+/**
+ * Adds 5px margin to the Home button if offcanvas opens for nicer spacing to the search bar
+ */
+rightOffcanvas.addEventListener("show.bs.offcanvas", (e) => {
+  homeItem.classList.add("mt-4");
+  alert("show");
+})
+
+/**
+ * Changes the collapse Directon for depending on a Breakpoint
+ */
 window.addEventListener("resize", (e) => {
   console.log(window.innerWidth)
-  if (window.innerWidth < 992 && largeScreen) {
+  if (window.innerWidth < rightNavbarBreakpoint && largeScreen) {
     leftSideBar.classList.remove("collapse-horizontal");
     largeScreen = false;
-  } else if (!largeScreen && window.innerWidth >= 992) {
+  } else if (!largeScreen && window.innerWidth >= rightNavbarBreakpoint) {
     leftSideBar.classList.add("collapse-horizontal");
     largeScreen = true;
   }
-
-
 })
