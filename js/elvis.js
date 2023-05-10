ed9df3deda23246dc4072f57bf864929f2fdb4e9
elvisbevab <elvis.bevab@mailbox.org>
2023-05-10 13:36:45 +0200

    on offcanvas close the topmargin of homebutton is removed

--- js/elvis.js
+++ js/elvis.js
@@ -1,41 +1,42 @@
-// Consts and Variables
+// Consts and Variables----------------------------------------------------------------
 
-
-
-// Navbar Items Right Bar
+// Navbar Right Side
 const rightOffcanvasToggler = document.querySelector("#rightOffcanvasToggler");
 const rightOffcanvas = document.querySelector("#tdNavbar");
-// console.log(rightOffcanvas);
 const leftCollapseToggler = document.querySelector("#leftCollapseToggler");
 const homeItem = document.querySelector("#homeItem")
 
 //Navbar left Side
 const leftSideBar = document.querySelector("#leftSideBar");
-
 const rightNavbarBreakpoint = 992;
 let largeScreen = window.innerWidth >= rightNavbarBreakpoint ? true : false;
 
-//Consts for Main Section
+//Main Section
 const submitBtnE = document.querySelector("#submitButton");
 submitBtnE.disabled = true;
 const taskInput = document.querySelector("#taskInput");
 
 
-//Event Listeners
-leftCollapseToggler.addEventListener("click", (e) => {
-  console.log(e.target);
-})
+//Event Listeners----------------------------------------------------------
 
 /**
- * Adds 5px margin to the Home button if offcanvas opens for nicer spacing to the search bar
+ * Adds 5px margin to the Home button if offcanvas opens. 
+ * Gives Nicer spacing to the search bar
  */
 rightOffcanvas.addEventListener("show.bs.offcanvas", (e) => {
   homeItem.classList.add("mt-4");
-  alert("show");
+})
+/**
+ * Removes 5px margin to the Home button when offcanvas closes. 
+ */
+rightOffcanvas.addEventListener("hide.bs.offcanvas", (e) => {
+  homeItem.classList.remove("mt-4");
 })
 
+
 /**
- * Changes the collapse Directon for depending on a Breakpoint
+ * Changes the collapse Directon of the left Sidebar
+ * depending on a Breakpoint
  */
 window.addEventListener("resize", (e) => {
   console.log(window.innerWidth)
