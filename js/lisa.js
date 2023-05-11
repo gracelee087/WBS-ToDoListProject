ed3efd44aa2103285876880a666b37a643b087af
elvisbevab <elvis.bevab@mailbox.org>
2023-05-11 18:09:44 +0200

    editing od tasks works

--- js/lisa.js
+++ js/lisa.js
@@ -6,6 +6,33 @@ let taskList = [];
 let selectedMenu = "tab-all";
 let filteredList = [];
 
+const addEditEvents = () => {
+  console.log("call Event adder")
+  const edits = document.querySelectorAll(".pencil")
+  edits.forEach(element => {
+    console.log(element);
+    element.addEventListener("click", (e) => {
+      edit(e);
+    })
+  })
+}
+const addInputvents = () => {
+  const inputs = document.querySelectorAll(".task input")
+  inputs.forEach(element => {
+    element.addEventListener("focusout", (e) => {
+      console.log("focusout")
+      console.log(element.dataset.task)
+      let task = getTaskById(element.dataset.task)
+      console.log("this task")
+      console.log(task)
+      console.log("after")
+      task.content = element.value
+      render()
+    })
+  })
+
+}
+
 addButton.addEventListener("mousedown", addTask);
 userInput.addEventListener("keydown", function (event) {
   if (event.keyCode === 13) {
@@ -44,24 +71,30 @@ function render() {
   for (let i = 0; i < list.length; i++) {
     if (list[i].isComplete) {
       result += `<div class="task task-done" id="${list[i].id}">
+      
             <span>${list[i].content}</span>
             <div class="button-box">
-            <button onclick="toggleDone('${list[i].id}')"><i class="fas fa-undo-alt"></i></button>
-            <button onclick="deleteTask('${list[i].id}')"><i class="fa fa-trash"></i></button>
+            <button class="check"onclick="toggleDone('${list[i].id}')"><i class="fas fa-undo-alt"></i></button>
+            <button class="trash" onclick="deleteTask('${list[i].id}')"><i class="fa fa-trash"></i></button>
             </div>
         </div>`;
     } else {
-      result += `<div class="task" id="${list[i].id}" >
+      result += `<div class="task" id="${list[i].id}" >            
             <span>${list[i].content}</span>
+            <input type="text" class="d-none" data-task="${list[i].id}">
             <div class="button-box">
-            <button onclick="toggleDone('${list[i].id}')"><i class="fa fa-check"></i></button>
-            <button onclick="deleteTask('${list[i].id}')"><i class="fa fa-trash"></i></button>
+            <button class="but pencil" value="${list[i].id}"><i class="bi bi-pencil"></i></button>
+            <button class="but check" onclick="toggleDone('${list[i].id}')"><i class="fa fa-check"></i></button>
+            <button class="but trash" onclick="deleteTask('${list[i].id}')"><i class="fa fa-trash"></i></button>
             </div>
         </div>`;
     }
+
   }
 
   document.getElementById("task-board").innerHTML = result;
+  addEditEvents();
+  addInputvents();
 }
 
 function toggleDone(id) {
@@ -115,3 +148,24 @@ function randomIDGenerator() {
   // after the decimal.
   return "_" + Math.random().toString(36).substr(2, 9);
 }
+
+const getTaskById = (id) => {
+  return taskList.filter((task) => task.id === id)[0];
+}
+
+const edit = (e) => {
+  console.log("call edit")
+  // console.log(e.currentTarget.value)
+  let id = e.currentTarget.value
+  let focus = document.querySelector(`#${id}`)
+  // console.log(focus)
+  let selectedSpanValue = focus.querySelector("span").textContent
+  let selectedInput = focus.querySelector("input")
+
+  selectedInput.value = selectedSpanValue;
+  focus.querySelector("span").classList.toggle("d-none")
+
+  focus.querySelector("input").classList.toggle("d-none")
+  selectedInput.focus()
+}
+
