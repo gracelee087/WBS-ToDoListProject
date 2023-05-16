dbb0b24e69b292123eda8a456edeeaf3ae603eb7
elvisbevab <elvis.bevab@mailbox.org>
2023-05-16 16:00:32 +0200

    bug fix

--- js/view/view.js
+++ js/view/view.js
@@ -25,26 +25,40 @@ export default class View {
       span.textContent = `${task.getContent}`
       const inp = document.createElement('input');
       const buttonBox = document.createElement("div")
+      /**
+       * Edit Button settings
+       */
       const pencil = document.createElement('button')
+      pencil.classList.add("but", "pencil");
+      pencil.innerHTML = "<i class=\"bi bi-pencil\"></i>"
+      /**
+       * Undo Button settings
+       */
       const undo = document.createElement('button')
+      undo.innerHTML = "<i class=\"fas fa-undo-alt\"></i>"
+
+      /**
+       * Trash Button settings
+       */
       const trash = document.createElement('button')
+      trash.classList.add("but", "trash");
       trash.setAttribute("value", `${task.getId}`)
+      trash.innerHTML = "<i class=\"fa fa-trash\">"
       trash.addEventListener("click", () => {
         this.remove(trash.getAttribute("value"))
       })
 
+      /**
+       * Check button settings
+       */
       const check = document.createElement('button')
-      pencil.innerHTML = "<i class=\"bi bi-pencil\"></i>"
-      undo.innerHTML = "<i class=\"fas fa-undo-alt\"></i>"
-      trash.innerHTML = "<i class=\"fa fa-trash\">"
+      check.classList.add("but", "check");
       check.innerHTML = "<i class=\"fa fa-check\"></i>"
 
-
       if (task.getIsComplete) {
+        tasDiv.classList.add("task-done")
         buttonBox.appendChild(undo)
         buttonBox.appendChild(trash)
-
-
       } else {
         buttonBox.appendChild(pencil)
         buttonBox.appendChild(check)
